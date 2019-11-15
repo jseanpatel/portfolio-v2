cd543a7ab421c565995acb04d0170c67bf413e6c
Jacob Patel <jseanpatel@gmail.com>
2019-11-14 23:22:47 -0800

    added index.vue skeleton

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,11 +1,14 @@
 <template>
-  
+    <div class="flex mb-4">
+    <div class="w-screen h-screen bg-gray-500">
+    </div>
+    </div>  
 </template>
 
 <script>
 export default {
-  name: "Project",
-  props: [],
+  name: "Landing",
+  props: ["title"],
   data() {
     return {
       message: "Try change me!"
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,27 @@
 <template>
   <Layout>
-    
-  
-    <project>
-    </project>
- 
+    <div id="landing">
+    </div>
+    <div id="about">
+    </div>
+    <div id="projects">
+    </div>
+    <div id="footer">
+    </div>
   </Layout>
 </template>
 
 <script>
 
 import TechnologyTag from '~/components/TechnologyTag.vue'
-import Project from '~/components/TechnologyTag.vue'
+import Project from '~/components/Project.vue'
+import Landing from '~/components/Landing.vue'
 
 export default {
   components: {
     TechnologyTag,
-    Project
+    Project,
+    Landing
   },
   metaInfo: {
     title: 'Hello, world!'
