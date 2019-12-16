a7f27172b3056b745cc03f84c548fd11da064a61
Jacob Patel <jseanpatel@gmail.com>
2019-12-15 23:20:29 -0800

    minor style changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -22,7 +22,7 @@
     <div id="about">
       <div class="flex justify-center w-full h-screen bg-background_gray">
         <div
-          class="self-center w-2/3 p-12 text-3xl text-center bg-white md:p-24 md:text-4xl h-half font-sanspro"
+          class="self-center w-2/3 p-12 text-3xl text-center bg-white rounded-lg md:p-24 md:text-4xl h-half font-sanspro"
         >
           <p>I am a student at the University of California, Berkeley where I am studying computer science (BA)</p>
         </div>
