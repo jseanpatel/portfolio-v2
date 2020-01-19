e2d5ec6ff657a5a413ea6c84e91812f72bcc1592
Jacob Patel <jseanpatel@gmail.com>
2020-01-18 20:11:36 -0800

    attempt on fa remote issues

--- gridsome.config.js
+++ gridsome.config.js
@@ -21,5 +21,45 @@ module.exports = {
         shouldPurgeUnusedKeyframes: true,
       }
     }
-  ]
+  ],
+  chainWebpack: config => {
+    config.module
+      .rule('css')
+      .oneOf('normal')
+      .use('postcss-loader')
+      .tap(options => {
+        options.plugins.unshift(...[
+          require('postcss-import'),
+          require('postcss-nested'),
+          require('tailwindcss'),
+        ])
+
+        if (process.env.NODE_ENV === 'production') {
+          options.plugins.push(...[
+            require('@fullhuman/postcss-purgecss')({
+              content: [
+                'src/assets/**/*.css',
+                'src/**/*.vue',
+                'src/**/*.js'
+              ],
+              extractors: [
+                {
+                  extractor: TailwindExtractor,
+                  extensions: ['css', 'vue', 'js']
+                }
+              ],
+              whitelist: ['svg-inline--fa'],
+              whitelistPatterns: [/shiki/, /fa-$/]
+            })
+          ])
+        }
+        return options
+      })
+  }
+}
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+  }
 }
