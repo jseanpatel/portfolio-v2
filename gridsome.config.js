c32385cfb1474cc1aaf24dec65a2d5d31227bec6
Jacob Patel <jseanpatel@gmail.com>
2020-01-19 00:17:18 -0800

    fixed white spacing issue and fa icon sizing issue

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,18 +4,21 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const purgecss = require("@fullhuman/postcss-purgecss");
-
 module.exports = {
   siteName: 'Gridsome',
   siteUrl: 'https://jseanpatel.github.io',
-  pathPrefix: '/Portfolio',
+  // pathPrefix: '/Portfolio',
   plugins: [
     {
       use: 'gridsome-plugin-tailwindcss',
       options: {
         tailwindConfig: './tailwind.config.js',
-        purgeConfig: {},
+        purgeConfig: {
+          whitelist: ['svg:not(:root).svg-inline--fa', 'body', 'html'],
+          whitelistPatterns: [/^fa-/, /^svg-inline--fa/],
+          whitelistPatternsChildren: [/^token/, /^pre/, /^code/],
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        },
         presetEnvConfig: {},
         shouldPurge: true,
         shouldImport: true,
@@ -32,49 +35,4 @@ module.exports = {
     "./src/**/*.pug",
     "./src/**/*.md",
   ],
-  whitelist: ["svg:not(:root).svg-inline--fa"],
-  whitelistPatterns: [/^fa-/, /^svg-inline--fa/],
-  whitelistPatternsChildren: [/^token/, /^pre/, /^code/],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  
-  chainWebpack: config => {
-    config.module
-      .rule('css')
-      .oneOf('normal')
-      .use('postcss-loader')
-      .tap(options => {
-        options.plugins.unshift(...[
-          require('postcss-import'),
-          require('postcss-nested'),
-          require('tailwindcss'),
-        ])
-
-        if (process.env.NODE_ENV === 'production') {
-          options.plugins.push(...[
-            require('@fullhuman/postcss-purgecss')({
-              content: [
-                'src/assets/**/*.css',
-                'src/**/*.vue',
-                'src/**/*.js'
-              ],
-              extractors: [
-                {
-                  extractor: TailwindExtractor,
-                  extensions: ['css', 'vue', 'js']
-                }
-              ],
-              whitelist: ['svg-inline--fa'],
-              whitelistPatterns: [/shiki/, /fa-$/]
-            })
-          ])
-        }
-        return options
-      })
-  }
-}
-
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-  }
 }
