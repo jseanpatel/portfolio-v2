fa13bd381894bac494d7475019c79a84bc333012
Jacob Patel <31494454+jseanpatel@users.noreply.github.com>
2020-08-14 18:44:33 -0700

    large layout changes and updated navbar

---
+++ src/components/NavigationItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <a :href="href" class="font-light hover:underline">{{name}}</a>
+</template>
+
+<script>
+import projects from "~/data/projects.json";
+
+export default {
+  name: "NavigationItem",
+  props: ["name", "href"],
+  data() {
+    return {
+      projects: projects,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
